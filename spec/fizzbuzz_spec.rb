1eba89326b2b01c53ef5e24d54c46cb910b2b7b4
JulieLev <jkgee1@yahoo.co.uk>
2016-09-28 20:37:30 +0100

    tests for 5 and 15, basic

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,28 @@ describe 'Fizzbuzz game' do
       expect(is_divisible_by_3?(1)).to be_falsey
     end
   end
+
+  context 'knows when a number is divisible by 5' do
+    it '5' do
+      expect(is_divisible_by_5?(5)).to be_truthy
+    end
+  end
+
+  context 'knows when a number is not divisible by 5' do
+    it 'not 5' do
+      expect(is_divisible_by_5?(1)).to be_falsey
+    end
+  end
+
+  context 'knows when a number is divisible by 15' do
+    it '15' do
+      expect(is_divisible_by_15?(15)).to be_truthy
+    end
+  end
+
+  context 'knows when a number is not divisible by 15' do
+    it 'not 15' do
+      expect(is_divisible_by_15?(1)).to be_falsey
+    end
+  end
 end
