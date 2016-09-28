418f7159d5aa5a6965c50c225ec8e01aca3c2a7d
JulieLev <jkgee1@yahoo.co.uk>
2016-09-28 21:11:32 +0100

    divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,23 @@
 require 'fizzbuzz'
 
 describe 'Fizzbuzz game' do
-  context 'knows when number is divisible by 3' do
+  context 'knows when number is divisible by' do
     it '3' do
       expect(is_divisible_by_3?(3)).to be_truthy
     end
+
+    it '5' do
+      expect(is_divisible_by_5?(5)).to be_truthy
+    end
   end
 
-  context 'knows when number is not divisible by 3' do
+  context 'knows when number is not divisible by' do
     it '3' do
       expect(is_divisible_by_3?(1)).not_to be_truthy
+    end
+
+    it '5' do
+      expect(is_divisible_by_5?(1)).not_to be_truthy
     end
   end
 end
