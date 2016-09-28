5ca50e2c45ddd6ea5d8b8b4af26cb9cf4d838da5
JulieLev <jkgee1@yahoo.co.uk>
2016-09-28 20:48:36 +0100

    refactors tests to dry out

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,38 +1,30 @@
 require 'fizzbuzz'
 
 describe 'Fizzbuzz game' do
-  context 'knows when a number is divisible by 3' do
+  context 'knows when a number is divisible by' do
     it '3' do
       expect(is_divisible_by_3?(3)).to be_truthy
     end
-  end
 
-  context 'knows when a number is not divisible by 3' do
-    it 'not 3' do
-      expect(is_divisible_by_3?(1)).not_to be_truthy
-    end
-  end
-
-  context 'knows when a number is divisible by 5' do
     it '5' do
       expect(is_divisible_by_5?(5)).to be_truthy
     end
-  end
 
-  context 'knows when a number is not divisible by 5' do
-    it 'not 5' do
-      expect(is_divisible_by_5?(1)).not_to be_truthy
-    end
-  end
-
-  context 'knows when a number is divisible by 15' do
     it '15' do
       expect(is_divisible_by_15?(15)).to be_truthy
     end
   end
 
-  context 'knows when a number is not divisible by 15' do
-    it 'not 15' do
+  context 'knows when a number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_3?(1)).not_to be_truthy
+    end
+
+    it '5' do
+      expect(is_divisible_by_5?(1)).not_to be_truthy
+    end
+
+    it '15' do
       expect(is_divisible_by_15?(1)).not_to be_truthy
     end
   end
